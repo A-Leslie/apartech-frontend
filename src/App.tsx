e7d5a3be585d855ef8df73acba13f7cb2c9f698b
A-Leslie <anneuhiriwe@gmail.com>
2023-08-18 17:28:51 +0200

    end

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
 
             if (profileObj) {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/users",
+                    "https://apartech-nnan.onrender.com/api/v1/users",
                     {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
@@ -124,7 +124,7 @@ function App() {
     return (
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://apartech-nnan.onrender.com/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
